c487a3100616c989b563fdf8829cb691fc82256f
reyteo8 <103292243+reyteo8@users.noreply.github.com>
2023-10-28 03:37:40 -0300

    0.1

--- FCC_tests/python_practice.py
+++ FCC_tests/python_practice.py
@@ -1,1 +1,8 @@
-print('Hello World')
+# inputs
+day = int(input('Insert day: '))
+month = int(input('Insert month: '))
+leap-year = input('Is it leap-year?(yes/no): ')
+
+# calcs
+if leap-year == 'yes':
+    x = 30 + 29
