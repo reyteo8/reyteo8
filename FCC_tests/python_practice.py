c3efbee57d0e5c08cbb3b008c1da29bf24b117d6
reyteo8 <103292243+reyteo8@users.noreply.github.com>
2023-10-30 10:55:35 -0300

    version 1

--- FCC_tests/python_practice.py
+++ FCC_tests/python_practice.py
@@ -3,6 +3,9 @@ day = int(input('Insert day: '))
 month = int(input('Insert month: '))
 leap_year = input('Is it leap-year?(yes/no): ')
 
+# set up calc
+x = 365 / 12
+
 # calcs
 if leap_year.upper() == 'yes':
     if month == 1:
@@ -16,8 +19,8 @@ if leap_year.upper() == 'no':
         number = day
     if month == 2:
         number = day + 31
-    if month == 3:
-        number = day + 31 + 28
+    if month >= 3:
+        number = day + (month - 1) * x
 else:
     print('error')
 # Finis
