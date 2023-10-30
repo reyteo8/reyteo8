64aa68a3bbe1417630bec18aa4c73e3cf873ce0d
reyteo8 <103292243+reyteo8@users.noreply.github.com>
2023-10-30 11:35:17 -0300

    correcting stuff

--- practice python/day_calc_V1.0.py
+++ practice python/day_calc_V1.0.py
@@ -7,21 +7,22 @@ leap_year = input('Is it leap-year?(yes/no): ')
 x = 365 / 12
 
 # calcs
-if leap_year.upper() == 'yes':
+if leap_year.upper() == 'YES':
     if month == 1:
         number = day
-    if month == 2:
+    elif month == 2:
         number = day + 31
-    if month >= 3:
+    elif month >= 3:
         number = day + (month - 2) * x + 28
-if leap_year.upper() == 'no':
+elif leap_year.upper() == 'NO':
     if month == 1:
         number = day
-    if month == 2:
+    elif month == 2:
         number = day + 31
-    if month >= 3:
+    elif month >= 3:
         number = day + (month - 2) * x + 28
 else:
     print('error')
+
 # Finis
 print('Days passed:', number, '/ 365')
