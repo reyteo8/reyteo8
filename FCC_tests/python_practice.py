dd86ce3b3fa8350f1a35b1e7b2fdf253b569c90b
reyteo8 <103292243+reyteo8@users.noreply.github.com>
2023-10-30 10:45:24 -0300

    first months

--- FCC_tests/python_practice.py
+++ FCC_tests/python_practice.py
@@ -4,5 +4,21 @@ month = int(input('Insert month: '))
 leap_year = input('Is it leap-year?(yes/no): ')
 
 # calcs
-if leap_year == 'yes':
-    x = 30 + 29
+if leap_year.upper() == 'yes':
+    if month == 1:
+        number = day
+    if month == 2:
+        number = day + 31
+    if month == 3:
+        number = day + 31 + 29
+if leap_year.upper() == 'no':
+    if month == 1:
+        number = day
+    if month == 2:
+        number = day + 31
+    if month == 3:
+        number = day + 31 + 28
+else:
+    print('error')
+# Finis
+print('Days passed:', number, '/ 365')
